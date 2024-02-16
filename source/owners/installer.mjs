b69dc6b077b991e41ec247d5b6a14ae42d59f11b
Alejandro CR <contacto@alejandrocr.co>
2024-02-15 22:35:18 -0500

    create owners context installer

---
+++ source/owners/installer.mjs
@@ -0,0 +1,30 @@
+/**
+ * @fileoverview Express application builder for the owners system.
+ */
+
+import { Installer } from '../adapters/express.mjs';
+import { route } from '../shared/http.mjs';
+import * as controllers from './controllers.mjs';
+import urls from './urls.mjs';
+
+
+const version = 'v1';
+const namespace = 'owners';
+
+
+class OwnersInstaller extends Installer {
+  /**
+   * Attaches the owners system to the application.
+   * @param {import('express').Express} application
+   */
+  install(application) {
+    application.get(route(urls.index, namespace, version), controllers.index);
+    application.post(route(urls.index, namespace, version), controllers.store);
+    application.get(route(urls.pointer, namespace, version), controllers.show);
+    application.put(route(urls.pointer, namespace, version), controllers.update);
+    application.delete(route(urls.pointer, namespace, version), controllers.destroy);
+  }
+}
+
+
+export default new OwnersInstaller();
